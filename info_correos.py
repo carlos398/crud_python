d7213938b5668a0120fbb67ed2139cc9d1d91945
carlos reyes <carlosarp2000@gmail.com>
2021-08-27 20:49:58 -0500

    informacion del  correo electronico

---
+++ info_correos.py
@@ -0,0 +1,22 @@
+def read_email():
+    # Get user email address
+    email = input("What is your email address?: ").strip()
+
+    # Slice out the user name
+    user_name = email[:email.index("@")]
+
+    # Slice the domain name
+    domain_name = email[email.index("@")+1:]
+
+    # Format message
+    output = "Your username is '{}' and your domain name is '{}'".format(user_name,domain_name)
+
+    # Display output message
+    print(output)
+
+def run():
+    read_email()
+
+
+if __name__ == "__main__":
+    run()
