db18a0771b728bce31bb78203012513011ac3755
carlos reyes <carlosarp2000@gmail.com>
2021-08-31 19:51:05 -0500

    agregar un archivo de texto con las cuentas

--- cuentas.py
+++ cuentas.py
@@ -1,4 +1,16 @@
+# -*- coding: utf-8 -*-
+
+
+import datetime
+
+
+def crear_archivo(ticket_de_consumo):
+    with open("./archivos/cuentas.txt", "a") as f:
+        f.write(ticket_de_consumo)
+
+
 def calculadora():
+    ticket = []
     total_descuentos = 0
     nombre_usuario = input('por favor digite su nombre: ')
     print('bienvenido {} '.format(nombre_usuario))
@@ -26,11 +38,21 @@ def calculadora():
     print('Gracias por usar nuestra calculadora de devengos =D ')
     print('\n'+'\n'+'\n'+'*'*30 + 'Ticket' + '*'*30)
     
+    date_now = datetime.datetime.now()
+    date_now = datetime.datetime.strftime(date_now, '%d/%m/%Y')
+    
+    with open("./archivos/cuentas.txt", "a") as f:
+        f.write(date_now)
+        
     for i in range(len(nombre_devengos)):
-        print('| se descontaron.... {dinero} que se gastaran en.... {nombre} '.format(
+        ticket.append('| se descontaron.... {dinero} que se gastaran en.... {nombre} '.format(
             dinero=dinero_devengo[i],
             nombre=nombre_devengos[i]
-            ))
+            )+'\n')
+        
+        crear_archivo(ticket[i])
+        
+        print(ticket[i])
         
     print('\n se descontaron en total.... {total_descuentos} \n su salario actual es.... {salario} '.format(
             total_descuentos=total_descuentos,
