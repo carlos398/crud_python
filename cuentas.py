56fd890177cb176de9cb2e69c6eac0c4fc43d9f2
carlos reyes <carlosarp2000@gmail.com>
2021-09-02 22:05:15 -0500

    comments for have an idea of what do his code

--- cuentas.py
+++ cuentas.py
@@ -5,22 +5,26 @@ import datetime
 
 
 def crear_archivo(ticket_de_consumo):
+    #this function gener a txt data for save all the taxes
     with open("./archivos/cuentas.txt", "a") as f:
         f.write(ticket_de_consumo)
 
 
 def calculadora():
+    # this part create the array, start the variable "total_descuentos" and ask for the name of the user
     ticket = []
     total_descuentos = 0
     nombre_usuario = input('por favor digite su nombre: ')
     print('bienvenido {} '.format(nombre_usuario))
     
+    # this part ask for te salary and ask if u want to make a discount in thath
     salario = int(input('Digite cual es su salario mensual: '))
     pregunta = input('Desea hacer un descuento? S/n : ')
     pregunta = pregunta.upper()
     nombre_devengos = []
     dinero_devengo = []
     
+    # if u write the "S" the program stars to ask u for the name of the discount and the cost of this
     while pregunta == 'S':
         nombre = input('Que nombre lleva este descuento?: ')
         cantidad = int(input('Cuanto dinero debe descontarse?: '))
